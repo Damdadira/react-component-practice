37fc6db36c86919a5227a4f9059726877caeb006
duddk <duddk@scatterx.io>
2024-11-25 16:26:57 +0900

    useEffect 컴포넌트 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
 import './App.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Board from './components/Board';
 import Counter from './components/Counter'; //useState
 
 function App() {
+  //useState
   const [ totalCount, setTotalCount ] = useState(0);
+
+  //useEffect - 무언가 변화가 있을 때 감지하여 반응하는 훅
+  const [ count, setCount ] = useState(0);
+  useEffect(() => { console.log(`${count}번 눌림.`) }, [count])
+
+  useEffect(() => {
+    console.log('컴포넌트 실행')
+    const handleScroll = () => { console.log(window.scrollY) };
+
+    document.addEventListener('scroll', handleScroll);
+    return () => document.removeEventListener('scroll', handleScroll);
+  }, []);
 
   return(
     <div>
+      <div style={{display: 'flex', flexDirection: 'column', gap: '1rem', margin: '1rem'}}>
+        <code style={{fontSize: '1.7rem', fontWeight: '800', color: 'orange'}}>useEffect</code>
+        <div>
+          <div>{count}번 누르셨습니다.</div>
+          <button onClick={() => setCount(count + 1)}>+</button>
+
+          {/* <div style={{height: 10000}}></div> */}
+        </div>
+      </div>
       <hr />
+
       <div style={{display: 'flex', flexDirection: 'column', gap: '1rem', margin: '1rem'}}>
         <code style={{fontSize: '1.7rem', fontWeight: '800', color: 'orange'}}>useState</code>
         <div>
